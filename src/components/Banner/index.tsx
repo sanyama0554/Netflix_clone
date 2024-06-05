8d5abdf0335344a86c50bd999571f6a7ca20485b
sanyama0554 <sanyama554@gmail.com>
2024-06-05 13:44:21 +0900

    Bannerコンポーネントファイルを作成

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,10 @@
+import { useProps } from "../Row/useProps";
+import Layout from "./Layout";
+
+const Banner = () => {
+  return (
+    return <Layout {...useProps()}/>;
+  );
+}
+
+export default Banner;
